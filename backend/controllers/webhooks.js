7a4df7ec21e47f53a96cc58f4d7a6e1d945f7cc5
Sawako99 <np05cp4a230095@iic.edu.np>
2025-06-19 14:36:08 +0545

    code correction

--- backend/controllers/webhooks.js
+++ backend/controllers/webhooks.js
@@ -5,9 +5,9 @@ import User from "../models/User.js"
 
 export const clerkWebhooks = async (req, res)=>{
     try {
-        const wjook = new Webhook(process.env.CLERK_WEBHOOK_SECRET)
+        const whook = new Webhook(process.env.CLERK_WEBHOOK_SECRET)
 
-        await wjook.verify(JSON.stringify(req.body), {
+        await whook.verify(JSON.stringify(req.body), {
             "svix-id": req.headers["svix-timestamp"],
             "svix-timestamp": req.headers["svix-timestamp"],
             "svix-signature": req.headers["svix-signature"]
