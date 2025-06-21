bd5eff578fd0f780029218b85486e563b27b2fc3
Sawako99 <np05cp4a230095@iic.edu.np>
2025-06-21 13:23:43 +0545

    backend complete

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,10 +1,15 @@
 import express from 'express';
-import { getUserData, purchaseCourse, UserEnrolledCourses } from '../controllers/userController.js';
+import { addUserRating, getUserCourseProgess, getUserData, purchaseCourse, updateUserCourseProgress, UserEnrolledCourses } from '../controllers/userController.js';
 
 const userRouter = express.Router()
 
 userRouter.get('/data', getUserData)
 userRouter.get('/enrolled-courses', UserEnrolledCourses)
 userRouter.get('/purchase', purchaseCourse)
+
+
+userRouter.post('update-course-progress', updateUserCourseProgress)
+userRouter.post('/get-course-progress', updateUserCourseProgress)
+userRouter.post('/add-rating', addUserRating)
 
 export default userRouter;
